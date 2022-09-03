a85b21025d4a57ea6ee6429fa58a79ca0cab37c1
Asadullah Khan <asadullahk15@gmail.com>
2022-09-03 15:42:24 +0500

    Database Connected

---
+++ mediumclone_nestjs/ormconfig.ts
@@ -0,0 +1,12 @@
+import { PostgresConnectionOptions } from 'typeorm/driver/postgres/PostgresConnectionOptions';
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: '@Summer1190',
+  database: 'mediumclone',
+};
+
+export default config;
