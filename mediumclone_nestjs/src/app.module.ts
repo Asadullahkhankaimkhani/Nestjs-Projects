5946d539f614c0d9e16d82556249c2c5011a8fc3
Asadullah Khan <asadullahk15@gmail.com>
2022-09-03 18:08:36 +0500

    User Module Controller Service

--- mediumclone_nestjs/src/app.module.ts
+++ mediumclone_nestjs/src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TagModule } from './tag/tag.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import config from 'ormconfig';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(config), TagModule],
+  imports: [TypeOrmModule.forRoot(config), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
