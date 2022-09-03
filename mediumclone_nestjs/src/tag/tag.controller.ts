7e1340b2b5664a03b337d35ef6b8e94526bce6b9
Asadullah Khan <asadullahk15@gmail.com>
2022-09-03 16:33:22 +0500

    CRUD WITH POSTGRESQL

--- mediumclone_nestjs/src/tag/tag.controller.ts
+++ mediumclone_nestjs/src/tag/tag.controller.ts
@@ -1,11 +1,43 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TagEntity } from './tag.entity';
 import { TagService } from './tag.service';
 
 @Controller('tag')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
   @Get('/')
-  getTags(): string[] {
-    return this.tagService.getTags();
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagService.findAll();
+  }
+
+  @Post('/')
+  async create(@Body() tag: TagEntity): Promise<TagEntity> {
+    return await this.tagService.create(tag);
+  }
+
+  @Get('/:id')
+  async findOne(@Param('id') id: number): Promise<TagEntity> {
+    return await this.tagService.findOne(id);
+  }
+
+  @Put('/:id')
+  async update(
+    @Param('id') id: number,
+    @Body() tag: TagEntity,
+  ): Promise<TagEntity> {
+    return await this.tagService.update(id, tag);
+  }
+
+  @Delete('/:id')
+  async delete(@Param('id') id: number): Promise<TagEntity> {
+    return await this.tagService.delete(id);
   }
 }
--- mediumclone_nestjs/src/tag/tag.service.ts
+++ mediumclone_nestjs/src/tag/tag.service.ts
@@ -1,8 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { TagEntity } from './tag.entity';
 
 @Injectable()
 export class TagService {
-  getTags(): string[] {
-    return ['tag1', 'tag2', 'tag3'];
+  constructor(
+    @InjectRepository(TagEntity) private tagRepository: Repository<TagEntity>,
+  ) {}
+
+  async findAll(): Promise<TagEntity[]> {
+    return await this.tagRepository.find();
+  }
+
+  async create(tag: TagEntity): Promise<TagEntity> {
+    return await this.tagRepository.save(tag);
+  }
+
+  async findOne(id: number): Promise<TagEntity> {
+    return await this.tagRepository.findOne({ where: { id } });
+  }
+
+  async update(id: number, tag: TagEntity): Promise<TagEntity> {
+    await this.tagRepository.update(id, tag);
+    return await this.tagRepository.findOne({ where: { id } });
+  }
+
+  async delete(id: number): Promise<TagEntity> {
+    const tag = await this.tagRepository.findOne({ where: { id } });
+    await this.tagRepository.delete(id);
+    return tag;
   }
 }
