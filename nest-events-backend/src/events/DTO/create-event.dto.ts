fb39d01a210daf51b943d53de2102de08172e093
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-09-29 23:34:45 +0500

    Input Validators

--- nest-events-backend/src/events/DTO/create-event.dto.ts
+++ nest-events-backend/src/events/DTO/create-event.dto.ts
@@ -1,6 +1,13 @@
+import { Length, IsDateString, IsString } from 'class-validator';
+
 export class CreateEventDto {
+  @IsString()
+  @Length(5, 255)
   name: string;
+  @Length(5, 255)
   description: string;
+  @IsDateString()
   when: Date;
+  @Length(5, 255)
   address: string;
 }
