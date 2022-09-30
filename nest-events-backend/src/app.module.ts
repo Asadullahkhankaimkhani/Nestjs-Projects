e9d983f7a17222f732fe139134041f19c36f9c0c
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-09-30 15:19:51 +0500

    Custom Configuration File and Options

--- nest-events-backend/src/app.module.ts
+++ nest-events-backend/src/app.module.ts
@@ -2,22 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Event } from './entities/events.entity';
+
 import { ConfigModule } from '@nestjs/config';
 import { EventsModule } from './events/events.module';
+import ormConfig from './config/orm.config';
+import ormConfigProd from './config/orm.config.prod';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.APP_HOST,
-      port: Number(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB,
-      entities: [Event],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [ormConfig],
+      expandVariables: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory:
+        process.env.NODE_ENV !== 'production' ? ormConfig : ormConfigProd,
     }),
     EventsModule,
   ],
---
+++ nest-events-backend/src/config/orm.config.prod.ts
@@ -0,0 +1,17 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Event } from 'src/entities/events.entity';
+import { registerAs } from '@nestjs/config';
+
+export default registerAs(
+  'orm.config',
+  (): TypeOrmModuleOptions => ({
+    type: 'postgres',
+    host: process.env.APP_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB,
+    entities: [Event],
+    synchronize: false,
+  }),
+);
