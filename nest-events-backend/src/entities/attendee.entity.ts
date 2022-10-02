52459ded5fa2e8b3ee2f12573352758208d65794
Asadullah Khan <41535758+Asadullahkhankaimkhani@users.noreply.github.com>
2022-10-02 16:36:59 +0500

    One to Many Relation

---
+++ nest-events-backend/src/entities/attendee.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Event } from './events.entity';
+
+@Entity()
+export class Attendee {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @ManyToOne(() => Event, (event) => event.attendee, {
+    nullable: false,
+  })
+  @JoinColumn()
+  // {
+  //     name: 'event_id',
+  //     referencedColumnName: 'secondary',
+  //   }
+  event: Event;
+}
